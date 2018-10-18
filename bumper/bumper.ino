b237ea7097f418e6b6a1a2a01e35c75a79ff2305
jimjamfelfam <jamie.feldman1+github@gmail.com>
2018-10-17 20:46:00 -0400

    added functions

--- bumper/bumper.ino
+++ bumper/bumper.ino
@@ -1,19 +1,68 @@
 void setup() {  
     // initialize inputs and outputs
-    pinMode(0, INPUT);
-    pinMode(1, INPUT);
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);         //forward right
+    pinMode(5, OUTPUT);         //forward left
+    pinMode(0, OUTPUT);         //backward right
+    pinMode(1, OUTPUT);         //backward left
+    attachInterrupt(digitalPinToInterrupt(2), left_back, RISING);  
+    attachInterrupt(digitalPinToInterrupt(3), right_back, RISING);
+    long time_pdeg;
 }
 
+// main vehichle path
 void loop() {
-    // read left and right
-    if(digitalRead(0)){
-        digitalWrite(2, HIGH);
-        digitalWrite(2, LOW);
+    forward(40);
+    turnright(90);
+    backward(50);
+    turnleft(90)
+    backward(40);
+    turnright(90);
+}
+
+// triggered on the left bumper 
+void left_back(){
+    if(digitalRead(2)&&digitalRead(3)){
+        backward(20);
     }
-    if(digitalRead(1)){
-        digitalWrite(3, HIGH);
-        digitalWrite(3, LOW);    
+    else{
+        digitalWrite(0,HIGH);
+        digitalWrite(1,HIGH);
+        delay(100);
+        digitalWrite(0,LOW);
+        delay(50);
+        digitalWrite(1,LOW);
     }
+}
+
+// triggered on the right bumper 
+void right_back(){
+    if(digitalRead(2)&&digitalRead(3)){
+        backward(20);
+    }
+    else{
+        digitalWrite(1,HIGH);
+        digitalWrite(0,HIGH);
+        delay(100);
+        digitalWrite(1,LOW);
+        delay(50);
+        digitalWrite(0,LOW);
+    }
+}
+
+// vehicle moves backwards
+void backwards(delay_time){
+    digitalWrite(0,HIGH);
+    digitalWrite(1,HIGH);
+    delay(delay_time);
+    digitalWrite(0,LOW);
+    digitalWrite(1,LOW);
+}
+
+//vehichle moves forwards
+void forwards(int delay_time){
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+    delay(delay_time);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
 }
