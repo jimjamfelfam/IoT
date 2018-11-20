143a9b0878db66dea95b1678dcd0b40aefa48dc3
jimjamfelfam <jamie.feldman1+github@gmail.com>
2018-11-20 01:05:01 -0500

    Desktop (#9)
    * sync DT (#6)
    
    * October 15th merge (#3)
    
    * Blink, hello world, and debounce
    
    * create bumper.ino
    
    * added functions
    
    * October 15th (#4)
    
    * Blink, hello world, and debounce
    
    * create bumper.ino
    
    * added functions
    
    * updates
    
    * POC (indy comps)

--- bumper/bumper.ino
+++ bumper/bumper.ino
@@ -1,68 +1,77 @@
+#include "Timer.h"
+long time_pdeg;
+long delay_time;
+long mod =1000;
+bool right_forward = LOW;
+bool left_forward = LOW;
+Timer l;
+Timer r;
+
 void setup() {  
     // initialize inputs and outputs
-    pinMode(4, OUTPUT);         //forward right
-    pinMode(5, OUTPUT);         //forward left
-    pinMode(0, OUTPUT);         //backward right
-    pinMode(1, OUTPUT);         //backward left
-    attachInterrupt(digitalPinToInterrupt(2), left_back, RISING);  
-    attachInterrupt(digitalPinToInterrupt(3), right_back, RISING);
-    long time_pdeg;
+    pinMode(22, OUTPUT);         // 1a
+    pinMode(23, OUTPUT);         // 2a
+    pinMode(24, OUTPUT);         // 3a
+    pinMode(25, OUTPUT);         // 4a
+    pinMode(26, OUTPUT);         // 1,2 EN
+    pinMode(27, OUTPUT);         // 3,4 EN
+    pinMode(18, INPUT);
+    pinMode(19, INPUT);
+    attachInterrupt(digitalPinToInterrupt(18), left_bump, RISING);  // left bumper
+    attachInterrupt(digitalPinToInterrupt(19), right_bump, RISING); // right bumper
 }
 
-// main vehichle path
+// main 
 void loop() {
-    forward(40);
-    turnright(90);
-    backward(50);
-    turnleft(90)
-    backward(40);
-    turnright(90);
+    l.update();
+    r.update();
 }
 
-// triggered on the left bumper 
-void left_back(){
-    if(digitalRead(2)&&digitalRead(3)){
-        backward(20);
-    }
-    else{
-        digitalWrite(0,HIGH);
-        digitalWrite(1,HIGH);
-        delay(100);
-        digitalWrite(0,LOW);
-        delay(50);
-        digitalWrite(1,LOW);
-    }
+// interrupts
+void right_bump(){
+    r.after(1*mod,r_stop);
+    digitalWrite(22,HIGH);
+    digitalWrite(23,LOW);
+    digitalWrite(26,HIGH);
+}
+void left_bump(){
+    l.after(1*mod,l_stop);
+    digitalWrite(24,HIGH);
+    digitalWrite(25,LOW);
+    digitalWrite(27,HIGH);
 }
 
-// triggered on the right bumper 
-void right_back(){
-    if(digitalRead(2)&&digitalRead(3)){
-        backward(20);
-    }
-    else{
-        digitalWrite(1,HIGH);
-        digitalWrite(0,HIGH);
-        delay(100);
-        digitalWrite(1,LOW);
-        delay(50);
-        digitalWrite(0,LOW);
-    }
+// forward and backwards functions
+void forward(){
+    l.after(1*mod,l_stop);
+    r.after(1*mod,r_stop);    
+    digitalWrite(24,LOW);
+    digitalWrite(25,HIGH);
+    digitalWrite(27,HIGH);
+    digitalWrite(22,LOW);
+    digitalWrite(23,HIGH);
+    digitalWrite(26,HIGH);        
+}
+void backward(){
+    l.after(1*mod,l_stop);
+    r.after(1*mod,r_stop);    
+    digitalWrite(24,HIGH);
+    digitalWrite(25,LOW);
+    digitalWrite(27,HIGH);
+    digitalWrite(22,HIGH);
+    digitalWrite(23,LOW);
+    digitalWrite(26,HIGH);        
 }
 
-// vehicle moves backwards
-void backwards(delay_time){
-    digitalWrite(0,HIGH);
-    digitalWrite(1,HIGH);
-    delay(delay_time);
-    digitalWrite(0,LOW);
-    digitalWrite(1,LOW);
+// stop functions
+void r_stop(){
+    digitalWrite(22,LOW);
+    digitalWrite(23,LOW);
+    digitalWrite(26,LOW);
+}
+void l_stop(){
+    digitalWrite(24,LOW);
+    digitalWrite(25,LOW);
+    digitalWrite(27,LOW);
 }
 
-//vehichle moves forwards
-void forwards(int delay_time){
-    digitalWrite(4,HIGH);
-    digitalWrite(5,HIGH);
-    delay(delay_time);
-    digitalWrite(4,LOW);
-    digitalWrite(5,LOW);
-}
