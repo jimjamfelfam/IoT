90dea12d55c718c52bc2fdf14865cea9a51d225c
jimjamfelfam <jamie.feldman1+github@gmail.com>
2018-11-27 15:13:02 -0500

    daily

--- build/sketch/oled_clock.ino.cpp
+++ build/sketch/oled_clock.ino.cpp
@@ -13,19 +13,22 @@
 Adafruit_SSD1306 display(OLED_RESET);
 
 /* WiFi Settings */
-#define ssid      "Jamie"
-#define password  "jimjamfelfam"
+const char* ssid     = "jamie";
+const char* password = "jimjamfelfam";
 
 int ledPin = 13;
 
-int timezone = 7 * 3600;
+int timezone = -5 * 3600;
 int dst = 0;
 
-#line 21 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
+time_t this_second =0;
+time_t last_second =0;
+
+#line 24 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
 void setup();
-#line 80 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
+#line 114 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
 void loop();
-#line 21 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
+#line 24 "d:\\OneDrive\\Documents\\IoT\\NodeMCU\\oled_clock\\oled_clock.ino"
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
@@ -46,13 +49,25 @@ void setup() {
   display.println( ssid );
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid,password);
- 
+  switch (WiFi.begin(ssid,password)){
+    case 3:
+      display.println("Connected");
+      break;
+    case 4:
+      display.println("Connection Failed");
+      break;
+    case 6:
+      display.println("Disconnected");
+      break;
+    default:
+      display.println("Default");
+      break;    
+  } 
   display.println("\nConnecting");
 
   display.display();
 
-  while( WiFi.status() != WL_CONNECTED ){
+  while( WiFi.status() == WL_DISCONNECTED){
       delay(500);
       display.print("."); 
       display.display();       
@@ -63,21 +78,40 @@ void setup() {
   display.display();
   display.setCursor(0,0);
   
-  display.println("Wifi Connected!");
-  display.print("IP:");
-  display.println(WiFi.localIP() );
+  
+  switch(WiFi.status()){
+    case 0:
+      display.println("Idle");
+      break;
+    case 1:
+      display.println("No SSID Avail");
+      break;
+    case 3:
+      display.println("Wifi Connected!");
+      display.print("IP:");
+      display.println(WiFi.localIP() );
+      break;
+    case 4:
+      display.println("Failed");
+      break;
+    case 6:
+      display.println("Disonnected");
+      break;
+  }
 
   display.display();
 
-  configTime(timezone, dst, "pool.ntp.org","time.nist.gov");
-  display.println("\nWaiting for NTP...");
+  delay(1000);
+
+  configTime(timezone, dst, "ca.pool.ntp.org", "time.nist.gov", "time1.google.com");
+  display.println("Waiting for NTP...");
 
   while(!time(nullptr)){
-     Serial.print("*");
-     
-     delay(1000);
+    display.print("*");
+    delay(1000);
   }
-  display.println("\nTime response....OK"); 
+
+  display.println("Time response....OK"); 
   display.display();  
   delay(1000);
 
@@ -107,33 +141,53 @@ void loop() {
   // Clear the buffer.
   display.clearDisplay();
  
-  display.setTextSize(3);
+  display.setTextSize(2);
   display.setTextColor(WHITE);
   
+  // hours : minutes
   display.setCursor(0,0);
   display.print(p_tm->tm_hour);
   display.print(":");
   if( p_tm->tm_min <10)
     display.print("0"); 
   display.print(p_tm->tm_min);
-  
-  display.setTextSize(2);
-  display.setCursor(90,5);
+
+  // seconds
+  display.setTextSize(1);
+  display.setCursor(57,0);
   display.print(".");
   if( p_tm->tm_sec <10)
     display.print("0"); 
   display.print(p_tm->tm_sec);
 
+  // day / month / year
   display.setTextSize(1);
-  display.setCursor(0,40);
+  display.setCursor(80,0);
   display.print(p_tm->tm_mday);
-  display.print("/");
+  display.print(".");
   display.print(p_tm->tm_mon + 1);
-  display.print("/");
-  display.print(p_tm->tm_year + 1900);
+  display.print(".");
+  display.print(p_tm->tm_year -100);
+
+  // connection status
+  display.setTextSize(1);
+  switch(WiFi.status()){
+    case 3:
+      display.setCursor(73,7);
+      display.print("Connected");
+      break;
+    default:
+      display.setCursor(55,7);
+      display.print("Disconnected");
+      break;
+  }
+
+  // analog read display
+  display.setTextSize(3);
+  display.setCursor(0,20);
+  display.print(analogRead(A0));
 
   display.display();
 
   delay(1000); // update every 1 sec
-
 }
